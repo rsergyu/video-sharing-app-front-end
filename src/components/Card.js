7fc1cce0cd9cc7099afaa5f850d0171d8673e604
Sergiu Radu <rsergyu@yahoo.com>
2023-03-22 17:36:04 +0200

    master

--- src/components/Card.js
+++ src/components/Card.js
@@ -83,7 +83,7 @@ const Card = ({type, video}) => {
 
   useEffect(() => {
     const fetchChannel = async ()=>{
-      const res = await axios.get(`https://video-share-app.onrender.com/api/users/find/${video.userId}`);
+      const res = await axios.get(`https://video-share-app.onrender.com/api/users/find/${video.userId}` , {withCredentials: true});
       setChannel(res.data);
     }
     fetchChannel();
